543c51d2136e90f8cd2047a24afc07b29faf8733
shioko <ichisadashioko@gmail.com>
2025-04-23 09:47:03 +0700

    scaffold

---
+++ Models/VGetAllCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PRN222_B5_PROJECT.Models;
+
+public partial class VGetAllCategory
+{
+    public string ParentProductCategoryName { get; set; } = null!;
+
+    public string? ProductCategoryName { get; set; }
+
+    public int? ProductCategoryId { get; set; }
+}
---
+++ Models/VProductAndDescription.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PRN222_B5_PROJECT.Models;
+
+public partial class VProductAndDescription
+{
+    public int ProductId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string ProductModel { get; set; } = null!;
+
+    public string Culture { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+}
